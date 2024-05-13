cd1ee3aaaa232d13f0066724411319cef2f29a2e
Taisuke Miyazaki <imishinist@gmail.com>
2024-05-13 12:01:40 +0900

    add message option

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fmt;
 use std::io;
 use std::process::{self, Command};
 
+use log;
 use clap::{Parser, ValueEnum};
 
 #[derive(Debug, Clone, Copy, ValueEnum)]
@@ -45,10 +46,17 @@ impl fmt::Display for Sound {
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about=None)]
+#[clap(group(clap::ArgGroup::new("notify-message")
+        .required(true)
+        .args(&["message", "command"])))]
 struct NotifyCmd {
     #[clap(short, long, default_value = "blow")]
     sound: Sound,
 
+    #[clap(short, long)]
+    message: Option<String>,
+
+    #[clap(name = "command")]
     rest: Vec<String>,
 }
 
@@ -62,7 +70,16 @@ enum NotifyError {
 }
 
 fn main() {
+    env_logger::init();
     let cmd = NotifyCmd::parse();
+    
+    if let Some(message) = cmd.message {
+        if let Err(e) = notify(None, &message, cmd.sound) {
+            let _ = notify(Some("Notify Failed"), format!("message: {} exit {:?}", message, e), cmd.sound);
+            process::exit(1);
+        }
+        return;
+    }
 
     let child_command = cmd.rest.join(" ");
     if let Err(e) = run_command(&child_command) {
@@ -82,6 +99,7 @@ fn main() {
 
 fn run_command(cmd: &str) -> Result<(), NotifyError> {
     let mut child = Command::new("sh").arg("-c").arg(cmd).spawn()?;
+    log::info!("run command: {:?}", cmd);
     let status = child.wait()?;
 
     if !status.success() {
@@ -94,6 +112,8 @@ fn run_command(cmd: &str) -> Result<(), NotifyError> {
 
 fn notify<S: AsRef<str>>(title: Option<&str>, message: S, sound: Sound) -> io::Result<()> {
     let title = title.unwrap_or("from notify");
+    log::info!("notify {{.title=\"{}\", .message={:?}}}", title, message.as_ref());
+
     let osascript = format!(
         r#"display notification "{}" with title "{}" sound name "{}""#,
         message.as_ref(),
